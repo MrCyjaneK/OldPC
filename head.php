472fdf172a3633cd5dc025b75b542f4bc7a63660
Czarek Nakamoto <mrcyjanek@gmail.com>
2020-06-10 00:44:38 +0200

    Added core files

---
+++ head.php
@@ -0,0 +1,4 @@
+<a href="http://fvqbo5pqjtehyuv5gyf67xkp5mbci2syn2637wssguufli7kh7gn4xqd.onion">D@rkn3t</a> | <a href="/games/">Games</a> | <a href="/files/index.php?p=/Documents/Scripts">Scripts</a> | <a href="/blog/">Blog</a> | <a href="/files/">Files</a> | <a href="/webide/">webide</a> | <a href="/status.php">Status</a>
+<?php
+
+?>
---
+++ index.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <title>OldPC</title>
+        <style>
+        body {
+            background-color: black;
+            color: white;
+            font-size: 150%;
+            margin:40px auto;
+            max-width:650px
+        }
+        a {
+            color: chartreuse;
+        }
+        pre {
+            white-space: pre-wrap;       /* Since CSS 2.1 */
+            white-space: -moz-pre-wrap;  /* Mozilla, since 1999 */
+            white-space: -pre-wrap;      /* Opera 4-6 */
+            white-space: -o-pre-wrap;    /* Opera 7 */
+            word-wrap: break-word;       /* Internet Explorer 5.5+ */
+        }
+        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <pre><code><?php include 'head.php'; ?>
+<h1>What is OldPC?</h1>
+OldPC is 'Hewlett-Packard HP Pavilion 17 Notebook PC' connected over ethernet cable to the <a href="https://en.wikipedia.org/wiki/IP_over_Avian_Carriers">IPoAC</a> router that gives us connection to the internet that is almost twice as fast as standard dial-up internet!
+In addition to that, it has an non-water cooling system with "Fan Always On" techonogy (that is impossible to disable in Basic Input/Output System) which keep us on about 40 degrees! It's CPU is 'AMD A8-4500M APU with Radeon(tm) HD Graphics', also, if you choose to use OldPC as your hosting provider, you will get:
+    > Random reboots
+    > Unstable and slow network connection
+    > Closed ports
+    > 600 GB of shared HDD storage
+    > 8 GB of RAM
+    > Access to camera, on which you can see the router, which is on my attic
+    > KVM based virtualization, with os of your choice
+    > No warranty of any kind.
+<hr />
+No, this is not a joke. Feel free to grab OldPC source from git and setup your own server.
+</code></pre>
+    </body>
+</html>
