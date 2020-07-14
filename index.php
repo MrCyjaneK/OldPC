9c402c37113fe7b735673f019d191d5e6f8e2628
Czarek Nakamoto <mrcyjanek@gmail.com>
2020-07-14 07:52:43 +0200

    We are no longer one large <code> block

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
             color: white;
             font-size: 150%;
             margin:40px auto;
-            max-width:650px
+            max-width:650px;
+            font-family: Monospace;
         }
         a {
             color: chartreuse;
@@ -23,21 +24,22 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
-        <pre><code><?php include 'head.php'; ?>
+        <?php include 'head.php'; ?>
 <img id="selfie" src="/selfie.php" width="100%"></img>
 <h1>What is OldPC?</h1>
-OldPC is 'Hewlett-Packard HP Pavilion 17 Notebook PC' connected over ethernet cable to the <a href="https://en.wikipedia.org/wiki/IP_over_Avian_Carriers">IPoAC</a> router that gives us connection to the internet that is almost twice as fast as standard dial-up internet!
-In addition to that, it has an non-water cooling system with "Fan Always On" techonogy (that is impossible to disable in Basic Input/Output System) which keep us on about 40 degrees! It's CPU is 'AMD A8-4500M APU with Radeon(tm) HD Graphics', also, if you choose to use OldPC as your hosting provider, you will get:
-    > Random reboots
-    > Unstable and slow network connection
-    > Closed ports
-    > 1600 <strike>600</strike> GB of shared HDD storage
-    > 7 <strike>8</strike> GB of RAM
-    > Access to camera, on which you can see the router, which is on my attic
-    > KVM based virtualization, with os of your choice
-    > Security provided by AMD Platform Security Processor
-    > No warranty of any kind.
-<hr /><center><?php
+OldPC is 'Hewlett-Packard HP Pavilion 17 Notebook PC' connected over ethernet cable to the <a href="https://en.wikipedia.org/wiki/IP_over_Avian_Carriers">IPoAC</a> router that gives us connection to the internet that is almost twice as fast as standard dial-up internet!<br />
+In addition to that, it has an non-water cooling system with "Fan Always On" techonogy (that is impossible to disable in Basic Input/Output System) which keep us on about 40 degrees! It's CPU is 'AMD A8-4500M APU with Radeon(tm) HD Graphics', also, if you choose to use OldPC as your hosting provider, you will get:<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> Random reboots<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> Unstable and slow network connection<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> Closed ports<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> 1600 <strike>600</strike> GB of shared HDD storage<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> 7 <strike>8</strike> GB of RAM<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> Access to camera, on which you can see the router, which is on my attic<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> KVM based virtualization, with os of your choice<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> Security provided by AMD Platform Security Processor<br />
+&nbsp;&nbsp;&nbsp;&nbsp;> No warranty of any kind.<br />
+<hr />
+<center><code><pre><?php
 date_default_timezone_set('Europe/Warsaw');
 function createCalendar($startingDay, $days, $month) {
     $spaces = '                        ';
@@ -74,10 +76,9 @@ function createCalendar($startingDay, $days, $month) {
     return implode(PHP_EOL,$out);
 }
 echo str_replace(date('d'),'<b style="color: cyan">'.date('d')."</b>",createCalendar(date('w')+2,date("t"),date("F Y")));
-?></center>
+?></pre></code></center>
 <hr />
-No, this is not a joke. Feel free to grab OldPC source from git and setup your own server.
-</code></pre>
+        No, this is not a joke. Feel free to grab OldPC source from git and setup your own server.
     <script>
     var nextid = 0;
     function prefetchNext() {
