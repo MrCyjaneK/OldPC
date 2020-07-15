1b928e2900372795b0161bd09e399d31c333c7a5
Czarek Nakamoto <mrcyjanek@gmail.com>
2020-07-15 23:59:24 +0200

    Fixed head

--- head.php
+++ head.php
@@ -1,4 +1,1 @@
-<a href="http://fvqbo5pqjtehyuv5gyf67xkp5mbci2syn2637wssguufli7kh7gn4xqd.onion">D@rkn3t</a> | <a href="/games/">Games</a> | <a href="/files/index.php?p=/Documents/Scripts">Scripts</a> | <a href="/blog/">Blog</a> | <a href="/files/">Files</a> | <a href="/webide/">webide</a> | <a href="/status.php">Status</a>
-<?php
-
-?>
+<center><a href="http://m7gobod5h4ubjwrdohogdhgrmtvzpcql2tz2dw3sjii76m5xsjtox6ad.onion/">D@rkn3t</a> | <a href="/games/">Games</a> | <a href="/blog/">Blog</a> | <a href="/files/">Files</a> | <a href="/status.php">Status</a></center>
